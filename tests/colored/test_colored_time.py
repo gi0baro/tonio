2ff560ea8c1a0f720e16a4828cd914283b01903c
Giovanni Barillari <giovanni.barillari@sentry.io>
2025-12-22 00:53:39 +0100

    Update time tests

--- tests/colored/test_colored_time.py
+++ tests/colored/test_colored_time.py
@@ -1,31 +1,46 @@
 import time
 
-from tonio.colored import spawn
-from tonio.colored.time import sleep, timeout
+import tonio.colored as tonio
+
+
+def test_time(run):
+    stack = []
+
+    async def _run():
+        stack.append(tonio.time.time())
+        await tonio.yield_now()
+        stack.append(tonio.time.time())
+
+    run(_run())
+    assert stack[1] > stack[0]
 
 
 def test_sleep(run):
-    async def f():
+    async def _run():
         start = time.monotonic()
-        await spawn(sleep(0.05), sleep(0.1))
+        await tonio.spawn(tonio.sleep(0.05), tonio.sleep(0.1))
         return time.monotonic() - start
 
-    assert run(f()) >= 0.1
+    assert run(_run()) >= 0.1
 
 
-async def _sleep(x):
-    await sleep(x)
-    return 3
+def test_timeout(run):
+    stack = []
 
+    async def _sleep(x):
+        await tonio.time.sleep(x)
+        stack.append(x)
+        return 3
 
-async def _test_timeout():
-    out1, success1 = await timeout(_sleep(0.2), 0.3)
-    out2, success2 = await timeout(_sleep(0.2), 0.1)
+    async def _run():
+        out1, success1 = await tonio.time.timeout(_sleep(0.2), 0.3)
+        out2, success2 = await tonio.time.timeout(_sleep(0.2), 0.1)
+        await tonio.time.sleep(1)
+        return (out1, out2, success1, success2)
+
+    out1, out2, success1, success2 = run(_run())
     assert out1 == 3
     assert out2 is None
     assert success1 is True
     assert success2 is False
-
-
-def test_timeout(run):
-    run(_test_timeout())
+    assert len(stack) == 1
