af0571d72d4276848ad2b69bd901ef1811960f21
Giovanni Barillari <giovanni.barillari@sentry.io>
2026-09-22 19:34:55 +0200

    Make I/O primitives public, re-export `io` module

--- tonio/colored/io.py
+++ tonio/colored/io.py
@@ -1,1 +1,8 @@
-from .._io import register as register
+import sys
+
+from .._colored._fd import FdStream as FdStream
+from .._io import ScheduledIO as ScheduledIO, register as register
+
+
+if sys.platform != 'win32':
+    from .._fd import Fd as Fd, ProcFd as ProcFd, open_proc_fd as open_proc_fd
